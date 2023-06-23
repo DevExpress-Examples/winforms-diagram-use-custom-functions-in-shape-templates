245f7f92c5e726e1b7dd8271a2ad2def996c9d3e
SH-Kirill <42139135+SH-Kirill@users.noreply.github.com>
2023-06-23 18:45:41 +0400

    Add files via upload

---
+++ CS/WindowsFormsApp4/Form1.cs
@@ -0,0 +1,65 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.Diagram.Core;
+using DevExpress.Utils.Serializing;
+using DevExpress.XtraDiagram;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+            CreateArcPoint.Register();
+            RegisterStencil();
+        }
+
+        void RegisterStencil()
+        {
+            var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("WindowsFormsApp4.CustomShapes.xml");
+            var stencil = DiagramStencil.Create("CustomStencil", "Custom Shapes", stream, shapeName => shapeName);
+            DiagramToolboxRegistrator.RegisterStencil(stencil);
+
+            diagramControl1.SelectedStencils = new StencilCollection() { "CustomStencil" };
+        }
+    }
+
+    public class CreateArcPoint : ICustomFunctionOperator {
+        private static readonly CreateArcPoint instance = new CreateArcPoint();
+
+        public static void Register() {
+            CriteriaOperator.RegisterCustomFunction(instance);
+        }
+        public static void Unregister() {
+            CriteriaOperator.UnregisterCustomFunction(instance);
+        }
+
+        public string Name => nameof(CreateArcPoint);
+
+        public Type ResultType(params Type[] operands) {
+            return typeof(double);
+        }
+
+        public object Evaluate(params object[] operands) {
+            if (operands.Length == 3
+                && operands[0] is double X
+                && operands[1] is double Y
+                && operands[2] is string axe) {
+
+                if (axe is "X")
+                    return X > 0.5 ? X : 0.5;
+                return X > 0.5 ? Y : 1;
+            }
+            else
+                return null;
+        }
+    }
+}
